a9fc6545e210970172205ffbe8a75e2cd291e236
P S <ps@Ps-MacBook-Pro.local>
2023-01-09 19:13:43 +0530

    :poop: added random base data

--- src/data.js
+++ src/data.js
@@ -1,14 +1,68 @@
 export const authorizedStock = [
     {
         class="Common",
+        pricePerShare=0,
+        exitValuation=0,
+
+    },
+    {
+        class="Series A1 Preferred",
+        pricePerShare=0,
+        exitValuation=0,
+        
+    },
+    {
+        class="Common",
+        pricePerShare=0,
+        exitValuation=0,
 
     },
     {
         class="Series A",
+        pricePerShare=0,
+        exitValuation=0,
+        
+    },
+    {
+        class="Common",
+        pricePerShare=0,
+        exitValuation=0,
+
+    },
+    {
+        class="Series A",
+        pricePerShare=0,
+        exitValuation=0,
         
     },
     {
         class="Series A1 Preferred",
+        pricePerShare=0,
+        exitValuation=0,
         
-    }
+    },
+    {
+        class="Series A",
+        pricePerShare=0,
+        exitValuation=0,
+        
+    },
+    {
+        class="Series A1 Preferred",
+        pricePerShare=0,
+        exitValuation=0,
+        
+    },
+    {
+        class="Common",
+        pricePerShare=0,
+        exitValuation=0,
+
+    },
+    {
+        class="Series A1 Preferred",
+        pricePerShare=0,
+        exitValuation=0,
+        
+    },
 ];
