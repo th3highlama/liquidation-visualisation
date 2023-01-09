75bf95340d7084bcf95c7ae604e5570096021149
P S <ps@Ps-MacBook-Pro.local>
2023-01-09 19:00:07 +0530

    :tada: removing boilerplate code

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const authorizedStock = [
+    {
+        class="Common",
+
+    },
+    {
+        class="Series A",
+        
+    },
+    {
+        class="Series A1 Preferred",
+        
+    }
+];
